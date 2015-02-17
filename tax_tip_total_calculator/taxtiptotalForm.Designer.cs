0dda8a937f649987c4a2b77708b0d8231fad695e
carole bacon <carolelynnbacon@gmail.com>
2015-02-17 12:48:41 -0800

    calculator update

---
+++ tax_tip_total_calculator/taxtiptotalForm.Designer.cs
@@ -0,0 +1,177 @@
+﻿namespace tax_tip_total_calculator
+{
+    partial class taxtiptotalForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.billLabel = new System.Windows.Forms.Label();
+            this.tipLabel = new System.Windows.Forms.Label();
+            this.taxLabel = new System.Windows.Forms.Label();
+            this.billTextBox = new System.Windows.Forms.TextBox();
+            this.tipTextBox = new System.Windows.Forms.TextBox();
+            this.taxTextBox = new System.Windows.Forms.TextBox();
+            this.calculateButton = new System.Windows.Forms.Button();
+            this.clearButton = new System.Windows.Forms.Button();
+            this.totalLabel = new System.Windows.Forms.Label();
+            this.totalTextBox = new System.Windows.Forms.TextBox();
+            this.exitButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // billLabel
+            // 
+            this.billLabel.AutoSize = true;
+            this.billLabel.Location = new System.Drawing.Point(29, 21);
+            this.billLabel.Name = "billLabel";
+            this.billLabel.Size = new System.Drawing.Size(138, 13);
+            this.billLabel.TabIndex = 0;
+            this.billLabel.Text = "Enter the amount of your bill";
+            // 
+            // tipLabel
+            // 
+            this.tipLabel.AutoSize = true;
+            this.tipLabel.Location = new System.Drawing.Point(29, 52);
+            this.tipLabel.Name = "tipLabel";
+            this.tipLabel.Size = new System.Drawing.Size(102, 13);
+            this.tipLabel.TabIndex = 1;
+            this.tipLabel.Text = "Enter the tip amount";
+            // 
+            // taxLabel
+            // 
+            this.taxLabel.AutoSize = true;
+            this.taxLabel.Location = new System.Drawing.Point(29, 80);
+            this.taxLabel.Name = "taxLabel";
+            this.taxLabel.Size = new System.Drawing.Size(138, 13);
+            this.taxLabel.TabIndex = 2;
+            this.taxLabel.Text = "Enter .07 for the tax amount";
+            // 
+            // billTextBox
+            // 
+            this.billTextBox.Location = new System.Drawing.Point(317, 18);
+            this.billTextBox.Name = "billTextBox";
+            this.billTextBox.Size = new System.Drawing.Size(100, 20);
+            this.billTextBox.TabIndex = 3;
+            // 
+            // tipTextBox
+            // 
+            this.tipTextBox.Location = new System.Drawing.Point(317, 49);
+            this.tipTextBox.Name = "tipTextBox";
+            this.tipTextBox.Size = new System.Drawing.Size(100, 20);
+            this.tipTextBox.TabIndex = 4;
+            // 
+            // taxTextBox
+            // 
+            this.taxTextBox.Location = new System.Drawing.Point(317, 77);
+            this.taxTextBox.Name = "taxTextBox";
+            this.taxTextBox.Size = new System.Drawing.Size(100, 20);
+            this.taxTextBox.TabIndex = 5;
+            // 
+            // calculateButton
+            // 
+            this.calculateButton.Location = new System.Drawing.Point(32, 162);
+            this.calculateButton.Name = "calculateButton";
+            this.calculateButton.Size = new System.Drawing.Size(75, 23);
+            this.calculateButton.TabIndex = 6;
+            this.calculateButton.Text = "Calculate";
+            this.calculateButton.UseVisualStyleBackColor = true;
+            this.calculateButton.Click += new System.EventHandler(this.calculateButton_Click);
+            // 
+            // clearButton
+            // 
+            this.clearButton.Location = new System.Drawing.Point(177, 162);
+            this.clearButton.Name = "clearButton";
+            this.clearButton.Size = new System.Drawing.Size(75, 23);
+            this.clearButton.TabIndex = 7;
+            this.clearButton.Text = "Clear";
+            this.clearButton.UseVisualStyleBackColor = true;
+            this.clearButton.Click += new System.EventHandler(this.clearButton_Click);
+            // 
+            // totalLabel
+            // 
+            this.totalLabel.AutoSize = true;
+            this.totalLabel.Location = new System.Drawing.Point(29, 124);
+            this.totalLabel.Name = "totalLabel";
+            this.totalLabel.Size = new System.Drawing.Size(31, 13);
+            this.totalLabel.TabIndex = 8;
+            this.totalLabel.Text = "Total";
+            // 
+            // totalTextBox
+            // 
+            this.totalTextBox.Location = new System.Drawing.Point(317, 121);
+            this.totalTextBox.Name = "totalTextBox";
+            this.totalTextBox.Size = new System.Drawing.Size(100, 20);
+            this.totalTextBox.TabIndex = 9;
+            // 
+            // exitButton
+            // 
+            this.exitButton.Location = new System.Drawing.Point(317, 162);
+            this.exitButton.Name = "exitButton";
+            this.exitButton.Size = new System.Drawing.Size(75, 23);
+            this.exitButton.TabIndex = 10;
+            this.exitButton.Text = "Exit";
+            this.exitButton.UseVisualStyleBackColor = true;
+            this.exitButton.Click += new System.EventHandler(this.exitButton_Click);
+            // 
+            // taxtiptotalForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(478, 197);
+            this.Controls.Add(this.exitButton);
+            this.Controls.Add(this.totalTextBox);
+            this.Controls.Add(this.totalLabel);
+            this.Controls.Add(this.clearButton);
+            this.Controls.Add(this.calculateButton);
+            this.Controls.Add(this.taxTextBox);
+            this.Controls.Add(this.tipTextBox);
+            this.Controls.Add(this.billTextBox);
+            this.Controls.Add(this.taxLabel);
+            this.Controls.Add(this.tipLabel);
+            this.Controls.Add(this.billLabel);
+            this.Name = "taxtiptotalForm";
+            this.Text = "TaxTipTotal Calculator";
+            this.Load += new System.EventHandler(this.taxtiptotalForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label billLabel;
+        private System.Windows.Forms.Label tipLabel;
+        private System.Windows.Forms.Label taxLabel;
+        private System.Windows.Forms.TextBox billTextBox;
+        private System.Windows.Forms.TextBox tipTextBox;
+        private System.Windows.Forms.TextBox taxTextBox;
+        private System.Windows.Forms.Button calculateButton;
+        private System.Windows.Forms.Button clearButton;
+        private System.Windows.Forms.Label totalLabel;
+        private System.Windows.Forms.TextBox totalTextBox;
+        private System.Windows.Forms.Button exitButton;
+    }
+}
+
---
+++ tax_tip_total_calculator/taxtiptotalForm.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+/*
+    Project      Tax Tip Total Calculator
+    Programmer   Carole Bacon
+    Date         1.22.2015
+                
+    	         Calculates the total amount of the bill, tip and tax
+*/
+namespace tax_tip_total_calculator
+{
+    public partial class taxtiptotalForm : Form
+    {
+        public taxtiptotalForm()
+        {
+            InitializeComponent();
+        }
+
+        //Declarations
+
+        double bill;        // the amount of the bill
+        double tip;         // the amount of the tip
+        double tax;         // the amount of the tax
+        double billPlusTip; // the amount of the bill + tip
+        double subtotal;    // the amount of the billPlusTip * .07
+        double total;       // the amount of the billPlusTip + the subtotal
+
+        private void taxtiptotalForm_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void clearButton_Click(object sender, EventArgs e)
+        {
+            // clear text boxes and output
+            billTextBox.Clear();
+            tipTextBox.Clear();
+            taxTextBox.Clear();
+            totalTextBox.Clear();
+        }
+
+        private void calculateButton_Click(object sender, EventArgs e)
+        {
+            // calculates the bill amount, tax and tip entered by user
+            totalTextBox.Text = "";
+
+            try
+            {
+                double bill, tip, tax, total;
+
+                bill = double.Parse(billTextBox.Text);
+                try
+                {
+                    tip = double.Parse(tipTextBox.Text);
+                    try
+                    {
+                        tax = double.Parse(taxTextBox.Text);
+                        billPlusTip = bill + tip;
+                        subtotal = billPlusTip * tax;
+                        total = billPlusTip + subtotal;
+                        totalTextBox.Text = total.ToString("c2");
+                    }
+                    // catches error with tax = double.Parse(taxTextBox.Text);
+                    catch
+                    {
+                        MessageBox.Show("Please enter .07 for the tax amount");
+                        taxTextBox.Focus();
+                        taxTextBox.SelectAll();
+                    }
+                }
+
+
+                // catches error with tip - double.Parse(tipTextBox.Text);
+                catch
+                {
+                    MessageBox.Show("Please enter the tip amount");
+                    tipTextBox.Focus();
+                    tipTextBox.SelectAll();
+                }
+                    
+                }
+             // catches error with bill - double.Parse(billTextBox.Text);
+                catch
+                {
+                    MessageBox.Show("Please enter the bill amount");
+                    billTextBox.Focus();
+                    billTextBox.SelectAll();
+                }
+            }
+
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+            // The user wants to exit the application. Close everything down.
+                 Application.Exit();
+ 
+        }
+
+            }
+        }
+
+   
